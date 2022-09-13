e548a32b67138f61d733d1c549c28e1d942818d8
Nazmul Hasan <nahasan169@gmail.com>
2022-09-13 17:10:45 +0600

    dynamically

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -46,124 +46,25 @@
     @stack('onpage-css')
 </head>
 <body>
-<!--*******************
-    Preloader start
-********************-->
 @include('layouts.partials.backend.preloader')
-<!--*******************
-        Preloader end
-    ********************-->
 <div id="main-wrapper">
-    <!--**********************************
-        Nav header start
-    ***********************************-->
+
     @include('layouts.partials.backend.header')
-    <!--**********************************
-            Nav header end
-        ***********************************-->
-
-    <!--**********************************
-        Header start
-    ***********************************-->
-    <!--**********************************
-        Sidebar start
-    ***********************************-->
     @include('layouts.partials.backend.left-sidebar')
-    <!--**********************************
-            Sidebar end
-        ***********************************-->
-
-    <!--**********************************
-        Content body start
-    ***********************************-->
     @yield('content')
-    <!--**********************************
-            Content body end
-        ***********************************-->
-    <!--**********************************
-        Footer start
-    ***********************************-->
+
     @include('layouts.partials.backend.footer')
-    <!--**********************************
-            Footer end
-        ***********************************-->
 </div>
-{{-- <div id="app">
-    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-        <div class="container">
-            <a class="navbar-brand" href="{{ url('/') }}">
-                {{ config('app.name', 'Laravel') }}
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                <span class="navbar-toggler-icon"></span>
-            </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav me-auto">
-
-                </ul>
-
-                <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ms-auto">
-                    <!-- Authentication Links -->
-                    @guest
-                        @if (Route::has('login'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                        @endif
-
-                        @if (Route::has('register'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
-                        @endif
-                    @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
-                                                 document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                    @endguest
-                </ul>
-            </div>
-        </div>
-    </nav>
-
-    <main class="py-4">
-        @yield('content')
-    </main>
-</div> --}}
-{{--ckeditor--}}
 <script src="{{asset('assets/plugin/ckeditor/ckeditor.js')}}"></script>
 <!-- Required vendors -->
 <script src="{{ asset('assets/backend/vendor/global/global.min.js')}}"></script>
 <script src="{{ asset('assets/backend/vendor/bootstrap-select/dist/js/bootstrap-select.min.js')}}"></script>
-{{-- <script src="{{ asset('assets/backend/vendor/chart.js/Chart.bundle.min.js')}}"></script> --}}
-
-<!-- Dashboard 1 -->
-{{-- <script src="{{ asset('assets/backend/js/dashboard/dashboard-1.js')}}"></script> --}}
-
-{{-- <script src="{{ asset('assets/backend/vendor/chart.js/Chart.bundle.min.js')}}"></script> --}}
 <script src="{{ asset('assets/backend/js/jquery-ui.js')}}"></script>
 <script src="{{ asset('assets/backend/js/custom.min.js')}}"></script>
 <script src="{{ asset('assets/backend/js/deznav-init.js')}}"></script>
 <script src="{{ asset('assets/backend/js/demo.js')}}"></script>
-<!-- laravel-toastr css -->
-{{-- <script src="http://cdn.bootcss.com/jquery/2.2.4/jquery.min.js"></script> --}}
+
 <script src="{{ asset('assets/backend/js/toastr.min.js')}}"></script>
 <!-- sweetalert2 css -->
 <script src="{{ asset('assets/backend/js/sweetalert2.min.js')}}"></script>
